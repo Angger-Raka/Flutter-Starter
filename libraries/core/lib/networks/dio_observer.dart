15994cc1275fb2b08dffe5afc21b892993835d44
Angger Raka Sanjaya <anggerrakasanjayapamungkas@gmail.com>
2023-11-13 13:38:14 +0700

    Update dependencies and remove unused files

--- libraries/core/lib/networks/dio_observer.dart
+++ libraries/core/lib/networks/dio_observer.dart
@@ -1,40 +1,79 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logger/logger.dart';
 
 class AppBlocObserver extends BlocObserver {
+  final _logger = Logger(
+    filter: null,
+    output: null,
+    printer: PrettyPrinter(
+      methodCount: 2,
+      errorMethodCount: 8,
+      lineLength: 120,
+      colors: true,
+      printEmojis: true,
+      printTime: false,
+    ),
+  );
+
   @override
   void onCreate(BlocBase bloc) {
     super.onCreate(bloc);
-    if (kDebugMode) print('onCreate -- ${bloc.runtimeType}');
+    _logger.log(
+      Level.info,
+      'onCreate -- ${bloc.runtimeType}',
+      stackTrace: StackTrace.empty,
+    );
   }
 
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    if (kDebugMode) print('onEvent -- ${bloc.runtimeType}, $event');
+    _logger.log(
+      Level.info,
+      'onEvent -- ${bloc.runtimeType}, $event',
+      stackTrace: StackTrace.empty,
+    );
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    if (kDebugMode) print('onChange -- ${bloc.runtimeType}, $change');
+    _logger.log(
+      Level.info,
+      'onChange -- ${bloc.runtimeType},\n currentState: ${change.currentState}\n nextState: ${change.nextState}',
+      stackTrace: StackTrace.empty,
+    );
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    if (kDebugMode) print('onTransition -- ${bloc.runtimeType}, $transition');
+    _logger.log(
+      Level.info,
+      'onTransition -- ${bloc.runtimeType},\n currentState: ${transition.currentState}\n nextState: ${transition.nextState}}',
+      stackTrace: StackTrace.empty,
+    );
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    if (kDebugMode) print('onError -- ${bloc.runtimeType}, $error');
+    _logger.log(
+      Level.error,
+      'onError -- ${bloc.runtimeType}',
+      error: error,
+      stackTrace: stackTrace,
+      time: DateTime.now(),
+    );
     super.onError(bloc, error, stackTrace);
   }
 
   @override
   void onClose(BlocBase bloc) {
     super.onClose(bloc);
-    if (kDebugMode) print('onClose -- ${bloc.runtimeType}');
+    _logger.log(
+      Level.info,
+      'onClose -- ${bloc.runtimeType}',
+      stackTrace: StackTrace.empty,
+    );
   }
 }
